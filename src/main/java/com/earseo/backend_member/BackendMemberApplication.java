9552669aa411332040c51ae7e3811634fa257e9d
manbron236 <minjuncho@naver.com>
2025-11-13 10:47:24 +0900

    [#1] feat : JPA Auditing 설정 및 BaseEntity 추가
    - 생성일, 수정일 자동관리 목적 @EnableJpaAuditing 추가
    - created_at, updated_at 필드 BaseEntity로 관리

--- src/main/java/com/earseo/backend_member/BackendMemberApplication.java
+++ src/main/java/com/earseo/backend_member/BackendMemberApplication.java
@@ -2,8 +2,10 @@ package com.earseo.backend_member;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class BackendMemberApplication {
 
 	public static void main(String[] args) {
