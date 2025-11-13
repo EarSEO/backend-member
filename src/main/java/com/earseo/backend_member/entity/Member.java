6564193258216a9e75519f23453c653f79bdfc63
manbron236 <minjuncho@naver.com>
2025-11-13 15:34:04 +0900

    [#1] feat : Member 엔티티 작성

---
+++ src/main/java/com/earseo/backend_member/entity/Member.java
@@ -0,0 +1,50 @@
+package com.earseo.backend_member.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "member")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class Member extends BaseEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long memberId;
+
+    @Column(length = 100, unique = true)
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @Column(length = 255)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Provider provider;
+
+    @Column(length = 50, nullable = false)
+    private String nickname;
+
+    @Column(length = 255)
+    private String profileImage;
+
+    @Column(length = 6)
+    private String gender;
+
+    @Column
+    private LocalDate birthdate;
+
+    @Column(length = 100)
+    private String nationality;
+
+
+}
