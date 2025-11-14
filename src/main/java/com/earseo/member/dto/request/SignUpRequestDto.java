5190e37ce24a7496d0593f8c6c0d71129b3d32d7
manbron236 <minjuncho@naver.com>
2025-11-14 16:27:35 +0900

    [#6] fix: 성별 Enum으로 관리, role 게스트 삭제

--- src/main/java/com/earseo/member/dto/request/SignUpRequestDto.java
+++ src/main/java/com/earseo/member/dto/request/SignUpRequestDto.java
@@ -1,5 +1,6 @@
 package com.earseo.member.dto.request;
 
+import com.earseo.member.entity.Gender;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -20,7 +21,7 @@ public record SignUpRequestDto(
         @Size(min = 2, max = 50, message = "닉네임은 2자 이상 50자 이하여야 합니다.")
         String nickname,
 
-        String gender,
+        Gender gender,
         LocalDate birthdate,
         String nationality
 ) {
