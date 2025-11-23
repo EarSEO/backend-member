8caa2ddd494d7300726210059666f73346152cc4
Jaewon Lee <58386334+jaewonLeeKOR@users.noreply.github.com>
2025-11-24 03:30:22 +0900

    [#12] feat : cicd 및 미들웨어 연결 설정

---
+++ src/main/java/com/earseo/member/common/config/KafkaConfig.java
@@ -0,0 +1,9 @@
+package com.earseo.member.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+
+@Configuration
+@EnableKafka
+public class KafkaConfig {
+}
