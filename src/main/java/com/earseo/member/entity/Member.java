f480006c48ddfeeecb68aac897d450d3fe3361a6
manbron236 <minjuncho@naver.com>
2025-11-28 04:11:34 +0900

    [hotfix] fix : helm value env 추가 및 멤버 엔티티 default_schema 제거

--- src/main/java/com/earseo/member/entity/Member.java
+++ src/main/java/com/earseo/member/entity/Member.java
@@ -6,7 +6,7 @@ import lombok.*;
 import java.time.LocalDate;
 
 @Entity
-@Table(name = "member", schema = "member")
+@Table(name = "member")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
