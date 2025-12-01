a98dbb13b95c81d1a455374310d35e90ec60d093
Min Jun Cho <minjuncho@naver.com>
2025-12-01 13:38:02 +0900

    [hotfix] fix : swagger

--- src/main/java/com/earseo/member/common/config/SecurityConfig.java
+++ src/main/java/com/earseo/member/common/config/SecurityConfig.java
@@ -37,7 +37,8 @@ public class SecurityConfig {
                         .requestMatchers("/swagger-ui/**",
                                 "/v3/api-docs/**",
                                 "/swagger-resources/**",
-                                "/webjars/**").permitAll()
+                                "/webjars/**",
+                                "/actuator/**").permitAll()
                         // USER
                         .requestMatchers("/api/user/member/**").permitAll()
                         // ADMIN
