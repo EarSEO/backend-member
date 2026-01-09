e6fecbd9107b55d65d42936f9d5804bb2e0a4b03
manbron236 <minjuncho@naver.com>
2026-01-09 14:45:18 +0900

    [#32] fix : 프로필 조회 API birthdate 응답 형식 변경

--- src/main/java/com/earseo/member/dto/response/ProfileResponseDto.java
+++ src/main/java/com/earseo/member/dto/response/ProfileResponseDto.java
@@ -1,6 +1,7 @@
 package com.earseo.member.dto.response;
 
 import com.earseo.member.entity.Gender;
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 import java.time.LocalDate;
 
@@ -10,6 +11,7 @@ public record ProfileResponseDto(
         String nickname,
         String profileImage,
         Gender gender,
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
         LocalDate birthdate,
         String nationality
 ) {
